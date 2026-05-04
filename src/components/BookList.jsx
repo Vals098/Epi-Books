04fafb071e92de96715dd372bc339c886b1b47aa
Vals098 <valeria.farinosi@gmail.com>
2026-05-04 15:45:08 +0200

    update: two col layout and state to parent element

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,39 +1,61 @@
 import { Component } from "react"
 import { Container, Row, Col, Form } from "react-bootstrap"
 import SingleBook from "./SingleBook"
+import CommentArea from "./CommentArea"
 
 class BookList extends Component {
   state = {
     search: "",
+    selectedAsin: null,
+  }
+
+  handleSelect = (asin) => {
+   return this.setState({ selectedAsin: asin })
   }
 
   render() {
     const filteredBooks = this.props.books.filter((book) =>
-        // to make the reserch case insensitive
+      // to make the reserch case insensitive
       book.title.toLowerCase().includes(this.state.search.toLowerCase()),
     )
 
     return (
       <Container>
-       
-        <Row className="justify-content-center mt-3">
-          <Col xs={12} md={6}>
-            <Form.Control
-              type="text"
-              placeholder="Search for a book title..."
-              value={this.state.search}
-              onChange={(e) => this.setState({ search: e.target.value })}
-            />
+        <Row>
+          {/* BOOK GRID */}
+          <Col xs={12} md={8}>
+            {/* search input */}
+            <Row className="justify-content-center mt-3">
+              <Col xs={12} md={6}>
+                <Form.Control
+                  type="text"
+                  placeholder="Search for a book title..."
+                  value={this.state.search}
+                  onChange={(e) => this.setState({ search: e.target.value })}
+                />
+              </Col>
+            </Row>
+            {/* books */}
+            <Row className="justify-content-center m-3 g-3">
+              {filteredBooks.map((book) => (
+                <Col xs={6} md={4} lg={3} key={book.asin}>
+                  {/* add onSelect and isSelected */}
+                  <SingleBook book={book} onSelect={this.handleSelect} isSelected={this.state.selectedAsin === book.asin}/>
+                </Col>
+              ))}
+            </Row>
           </Col>
-        </Row>
-
-
-        <Row className="justify-content-center m-3 g-3">
-          {filteredBooks.map((book) => (
-            <Col xs={6} md={4} lg={3} key={book.asin}>
-              <SingleBook book={book} />
-            </Col>
-          ))}
+          {/* COMMENT AREA */}
+          <Col xs={12} md={4}>
+            {/* if selected is true show comment area */}
+            <div className="mt-3">
+              {this.state.selectedAsin ? (
+                <CommentArea asin={this.state.selectedAsin} />
+              ) : (
+                <p className="text-muted">Select a book to see comments</p>
+              )}
+            </div>
+          </Col>
         </Row>
       </Container>
     )
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,16 +1,8 @@
 import { Component } from "react"
 import { Card, Button } from "react-bootstrap"
-import CommentArea from "./CommentArea"
+
 
 class SingleBook extends Component {
-  state = {
-    selected: false,
-  }
-  toggleSelected = () => {
-    this.setState({
-      selected: !this.state.selected,
-    })
-  }
 
   render() {
     const book = this.props.book
@@ -18,13 +10,13 @@ class SingleBook extends Component {
     return (
       <>
         <Card
-          onClick={this.toggleSelected}
+          onClick={() => this.props.onSelect(book.asin)}
           className="h-100 d-flex flex-column"
           style={{
-            boxShadow: this.state.selected
+            boxShadow: this.props.isSelected
               ? "0 4px 12px rgba(0, 128, 0, 0.5)"
               : "none",
-            border: this.state.selected
+            border: this.props.isSelected
               ? "1px solid rgba(0, 128, 0, 0.7)"
               : "1px solid grey",
             transition: "all 0.3s ease",
@@ -39,14 +31,7 @@ class SingleBook extends Component {
             <Button variant="success" className="mt-auto">
               Add to cart
             </Button>
-            {/* if selected is true show comment area */}
-            <div className="mt-3">
-              {this.state.selected && (
-                <div className="mt-3">
-                  <CommentArea asin={book.asin} />
-                </div>
-              )}
-            </div>
+      
           </Card.Body>
         </Card>
       </>
