4caf412d17ca5c57f4141aff7b43d9924540096c
Vals098 <valeria.farinosi@gmail.com>
2026-05-07 16:19:43 +0200

    Point 1

---
+++ src/tests/Welcome.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react"
+import Welcome from "../components/Welcome"
+import { describe, it, expect } from "vitest"
+
+describe("Welcome", () => {
+    it('checks if the component mounts correctly', () =>{
+        // 1) montaggio del componente nel DOM
+        render(<Welcome/>)
+        // 2) ricerca
+        const welcomeComponent = screen.getByText(/welcome/i)
+        // 3) interazione con l'elemento trovato (opz)
+        // 4) verifica
+        expect(welcomeComponent).toBeInTheDocument()
+    })
+})
