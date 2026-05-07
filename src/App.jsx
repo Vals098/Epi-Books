2e4b3c961e90169911a0d56185908948966f2dc6
Vals098 <valeria.farinosi@gmail.com>
2026-05-07 16:02:59 +0200

    test workspace setup

--- src/App.jsx
+++ src/App.jsx
@@ -13,36 +13,37 @@ import Welcome from "./components/Welcome"
 // import AllTheBooks from "./components/AllTheBooks"
 import books from "./data/fantasy.json"
 import BookList from "./components/BookList"
-
+import { BrowserRouter, Routes, Route } from "react-router"
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <header>
-        <MyNav></MyNav>
-      </header>
-      {/* without react-bootstrap */}
-      {/* <div className="container">
+    <BrowserRouter>
+      <div className="d-flex flex-column min-vh-100">
+        <header>
+          <MyNav></MyNav>
+        </header>
+        {/* without react-bootstrap */}
+        {/* <div className="container">
     <div className="row">
       <div className="col"></div>
     </div>
   </div> */}
-      <main className="flex-grow-1">
-        <Welcome></Welcome>
-        {/* with react-bootstrap */}
-        <Container>
-          <Row>
-            <Col>
-              <BookList books={books} />
-
-            </Col>
-          </Row>
-        </Container>
-      </main>
-      <footer>
-        <MyFooter></MyFooter>
-      </footer>
-    </div>
+        <main className="flex-grow-1">
+          <Welcome></Welcome>
+          {/* with react-bootstrap */}
+          <Container>
+            <Row>
+              <Col>
+                <BookList books={books} />
+              </Col>
+            </Row>
+          </Container>
+        </main>
+        <footer>
+          <MyFooter></MyFooter>
+        </footer>
+      </div>
+    </BrowserRouter>
   )
 }
 
